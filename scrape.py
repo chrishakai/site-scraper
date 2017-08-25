4514af6bd2aab5a6a776bd7f7d3ce1abf1cb6b09
Chris Davis <chris.davis@hakai.org>
2017-08-25 13:54:26 -0700

    Tidy up and error handling

--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@ import re
 import time
 import urllib2,cookielib
 from bs4 import BeautifulSoup, SoupStrainer
+
 sitemap= 'https://www.hakaimagazine.com/sitemap.xml'
 #sitemap= 'http://localhost/sitemap_one.xml'
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -37,44 +38,50 @@ for idx, link in enumerate(links):
         directory += '/' + m.group(2)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    if not os.path.exists(directory + '/img'):
-        os.makedirs(directory + '/img')
+
+
+    print '\033[92m' + 'Starting page ' + directory + '\033[0m'
 
     # Request the page
     req = urllib2.Request(link.string, headers=hdr)
     try:
         page = urllib2.urlopen(req)
+        htmlpage = page.read()
+        soup = BeautifulSoup(htmlpage, 'html.parser')
+
+        # Write the file
+        contents = open(directory + '/contents.html', 'w')
+        contents.write(htmlpage)
+        contents.close()
+        print 'Write file ' + '\033[94m' + directory + '/contents.html' + '\033[0m' + ': ' + '\033[93m' + htmlpage[:15] + '...' + '\033[0m'
+
+        # Request source images
+        imglinks = soup.find_all('source')
+        imgfiles = []
+        for image in imglinks:
+            imgname = re.search('https:\/\/www.hakaimagazine.com\/sites\/default\/files\/styles\/[\w]+\/[\w]+\/([-\w.\/]+)', image.get('srcset').split(',')[0])
+            imgfiles.append(imgname.group(1))
+        imgfiles = list(set(imgfiles))
+        for image in imgfiles:
+            if not os.path.exists(directory + '/img'):
+                os.makedirs(directory + '/img')
+            imagename = re.search('(?:.+\/)*([-\w]+\..+)', image).group(1)
+            req = urllib2.Request('https://www.hakaimagazine.com/sites/default/files/' + image, headers=hdr)
+            try:
+                f = open(directory + '/img/' + imagename,'wb')
+                imagefile = urllib2.urlopen(req).read()
+                f.write(imagefile)
+                f.close()
+                print 'Write image ' + '\033[94m' + directory + '/img/' + imagename + '\033[0m'
+            except urllib2.HTTPError, e:
+                print '\033[91m' + e.fp.read() + '\033[0m'
+        # Request twitter images
+
+        # Request facebook images
+
     except urllib2.HTTPError, e:
         print e.fp.read()
 
-    # Write the file
-    print '\033[92m' + 'Starting page ' + directory + '\033[0m'
-    htmlpage = page.read()
-    soup = BeautifulSoup(htmlpage, 'html.parser')
-
-    # Request source images
-    imglinks = soup.find_all('source')
-    imgfiles = []
-    for image in imglinks:
-        imgname = re.search('https:\/\/www.hakaimagazine.com\/sites\/default\/files\/styles\/[\w]+\/[\w]+\/([-\w.\/]+)', image.get('srcset').split(',')[0])
-        imgfiles.append(imgname.group(1))
-    imgfiles = list(set(imgfiles))
-    for image in imgfiles:
-        imagename = re.search('(?:.+\/)*([-\w]+\..+)', image).group(1)
-        f = open(directory + '/img/' + imagename,'wb')
-        print 'Write image ' + '\033[94m' + directory + '/img/' + imagename + '\033[0m'
-        req = urllib2.Request('https://www.hakaimagazine.com/sites/default/files/' + image, headers=hdr)
-        f.write(urllib2.urlopen(req).read())
-        f.close()
-    # Request twitter images
-
-    # Request facebook images
-
-    # Write the file
-    contents = open(directory + '/contents.html', 'w')
-    print 'Write file ' + '\033[94m' + directory + '/contents.html' + '\033[0m' + ': ' + '\033[93m' + htmlpage[:15] + '\033[0m'
-    contents.write(htmlpage)
-    contents.close()
     print '\033[91m' + str(idx + 1) + ' of ' + str(len(links)) + ' done -----------------------------------------------------' + '\033[0m'
     # Take a nap so we don't overload the server with many many requests
     time.sleep(0.5)
